0c2887277bc4c725857e531171e32768000abb98
loi <loi.nguyen@xomad.com>
2024-12-01 11:19:49 +0700

    Feature/Publish my second brain

---
+++ content/index.md
@@ -0,0 +1,5 @@
+---
+title: My Second Brain
+---
+
+Welcome to my Second Brain! This is a constantly evolving collection of notes, ideas, projects, and resources.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "👩‍💻 Loi Nguyen Second Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "loinguyen3108.github.io/my-second-brain",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
